8adfcdfc9b9daa611584b7c50a0a1f6425225ab4
gitcoin_yoon <socozy7210@gmail.com>
2022-04-27 00:16:33 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d590c7981af0068b345c.js"
+    "url": "webpack-runtime-a7381ed6897e56afd594.js"
   },
   {
     "url": "styles.f68c5166982f19de8306.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c0eabb5485d9cc553c81fa2227f1ffa1"
+    "revision": "69f8d40d7b0eaa89bab260de5013c56f"
   },
   {
     "url": "polyfill-c5f50f2ad13861331c8a.js"
